86d31d6567b560362698024c43963cd27aedf1ed
KennyHooper <kennyhooper@gmail.com>
2016-07-19 12:33:52 -0700

    experimentation

--- pages/js/js.js
+++ pages/js/js.js
@@ -1,7 +1,3 @@
-// var firstName = prompt("First Name?");
-// var lastName = prompt("Last Name?");
-// var fullName = firstName + lastName;
-//
-// var name = document.querySelector("#name");
-//
-// name.textContent(fullName);
+var firstName = "Kenny";
+var lastName = "Hooper";
+console.log(firstName);
